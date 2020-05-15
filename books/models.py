e13d2601e7d1be1b155cb6fb2d6069439f2a67e0
susil <thapasusil53@gmail.com>
2020-05-15 13:52:54 +0545

    ch12

--- books/models.py
+++ books/models.py
@@ -2,12 +2,16 @@ from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
 import uuid
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from PIL import Image
 
 class Books(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
   title = models.CharField(max_length=200)
   author = models.CharField(max_length=200)
   price = models.DecimalField(max_digits=6, decimal_places=2)
+  cover = models.ImageField(upload_to='covers/', blank=True)
 
   class Meta:
     verbose_name_plural = "Books"
@@ -17,7 +21,16 @@ class Books(models.Model):
 
   def get_absolute_url(self):
       return reverse("book_detail", args=[str(self.id)])
-
+  
+  def save(self, *args, **kwargs):
+    super(Books, self).save(*args, **kwargs)
+    
+    img = Image.open(self.cover.path)  # opens the image of current instance
+    
+    if img.height > 400 and img.width > 400:
+      output_size = (400, 400)
+      img.thumbnail(output_size)
+      img.save(self.cover.path)
 
 class Review(models.Model):
   book = models.ForeignKey(Books, on_delete=models.CASCADE, related_name='reviews')
--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -182,4 +182,6 @@ STATICFILES_FINDERS = [
 "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
