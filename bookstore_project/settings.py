218e3e410c8ce6179d652e97900876cdfd5c43f9
susil <thapasusil53@gmail.com>
2020-05-21 15:21:22 +0545

    17

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -23,10 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+#should be default to production
+ENVIRONMENT = config('ENVIRONMENT', default= 'development')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -200,3 +203,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # django-debug-toolbar
 INTERNAL_IPS = ['127.0.0.1',]
+
+# Production
+if ENVIRONMENT == 'production':
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 3600 
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True 
+    SECURE_HSTS_PRELOAD = True 
+    SECURE_CONTENT_TYPE_NOSNIFF = True 
+    SESSION_COOKIE_SECURE = True 
+    CSRF_COOKIE_SECURE = True
+    SECURE_REFERRER_POLICY= 'origin'
--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('bookstore-admin/', admin.site.urls),
 
     # User management
     path('accounts/', include('allauth.urls')),
