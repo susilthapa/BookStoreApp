ecef83eb7b1d095e155ef1557f8bd5a1a628af85
susil <thapasusil53@gmail.com>
2020-05-23 00:26:32 +0545

    setting

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -225,4 +225,4 @@ if ENVIRONMENT == 'production':
 # Heroku
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['defaults'].update(db_from_env)
+DATABASES.update(default=db_from_env)
