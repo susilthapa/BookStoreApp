f1b8e9333407af3e6f80e7876649800cde24469a
susil <thapasusil53@gmail.com>
2020-08-29 18:10:23 +0545

    main urls.py file modified

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -17,9 +17,10 @@ urlpatterns = [
     path('orders/', include('orders.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls))
